eaf5dcc3aa62c736ae768691025cadd54c8fd16d
Pascal Sikorski <pascalsikorskips@gmail.com>
2024-10-19 18:18:44 -0500

    Update DES.cpp

--- DES.cpp
+++ DES.cpp
@@ -6,27 +6,64 @@
 using namespace std;
 
 string encryption(string plaintext, vector<string> keys) {
-    //Initial permutation here
-    string left = plaintext.substr(0,32);
-    string right = plaintext.substr(32,32);
-    //16 rounds below
-    for (int i = 0; i < 16; i++){
-        string ogRight = right;
-        right = expansion(right);
-        // right = XOR(right, subkey[i]);
-        right = sBox(right);
-        right = pBox(right);
-        // left = XOR(left, right);
-        string ogLeft = left;
-        left = ogRight;
-        right = ogLeft;
+    string initialPermutatedText = "";
+    for (int i = 0; i < 64; i++) {
+        initialPermutatedText += plaintext[IP[i] - 1];
     }
-    string ciphertext = left + right;
-    //Final permutation here
+
+    // 32, 32
+    string left = initialPermutatedText.substr(0, 32);
+    string right = initialPermutatedText.substr(32, 32);
+
+    // 16 rounds
+    for (int i = 0; i < 16; i++) {
+        string prevLeft = left;
+        string expandedRight = expansion(right);
+        string XORED = XOR(expandedRight, keys[i], 48);
+        string SBOXED = sBox(XORED);
+        string PBOXED = pBox(SBOXED);
+        
+        left = right;
+        right = XOR(prevLeft, PBOXED, 32);
+    }
+
+    // should be right order here?
+
+    string combined = right + left;
+    string ciphertext = "";
+    for (int i = 0; i < 64; i++) {
+        ciphertext += combined[FP[i] - 1];
+    }
     return ciphertext;
 }
 
 string decryption(string encrypted, vector<string> keys) {
-    string plaintext;
+    string initialPermutatedText = "";
+    for (int i = 0; i < 64; i++) {
+        initialPermutatedText += encrypted[IP[i] - 1];
+    }
+
+    // 32, 32
+    string left = initialPermutatedText.substr(0, 32);
+    string right = initialPermutatedText.substr(32, 32);
+
+    // 16 rounds (but -- reversed!)
+    for (int i = 15; i >= 0; i--) {
+        string prevLeft = left;
+        string expandedRight = expansion(right);
+        string XORED = XOR(expandedRight, keys[i], 48);
+        string SBOXED = sBox(XORED);
+        string PBOXED = pBox(SBOXED);
+        
+        left = right;
+        right = XOR(prevLeft, PBOXED, 32);
+    }
+
+    string combined = right + left;
+
+    string plaintext = "";
+    for (int i = 0; i < 64; i++) {
+        plaintext += combined[FP[i] - 1];
+    }
     return plaintext;
 }
